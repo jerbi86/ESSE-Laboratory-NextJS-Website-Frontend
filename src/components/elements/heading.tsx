1c8001f63e8100ed7538c1efaa8009a5169ef906
jerbi86 <ahmedjerbi86@gmail.com>
2025-11-07 03:01:18 +0100

    Refactor Heading component to improve type definitions and enhance code clarity

--- src/components/elements/heading.tsx
+++ src/components/elements/heading.tsx
@@ -1,7 +1,15 @@
 import { cn } from "@/lib/utils";
-import { AnimationProps, MotionProps } from "framer-motion";
+import type { MotionProps } from "framer-motion";
 import React from "react";
 import Balancer from "react-wrap-balancer";
+
+type HeadingProps = {
+  className?: string;
+  as?: React.ElementType;
+  children: React.ReactNode;
+  size?: "sm" | "md" | "xl" | "2xl";
+} & MotionProps &
+  React.HTMLAttributes<HTMLHeadingElement>;
 
 export const Heading = ({
   className,
@@ -9,20 +17,14 @@ export const Heading = ({
   children,
   size = "md",
   ...props
-}: {
-  className?: string;
-  as?: any;
-  children: any;
-  size?: "sm" | "md" | "xl" | "2xl";
-  props?: React.HTMLAttributes<HTMLHeadingElement | AnimationProps>;
-} & MotionProps &
-  React.HTMLAttributes<HTMLHeadingElement | AnimationProps>) => {
-  const sizeVariants = {
+}: HeadingProps) => {
+  const sizeVariants: Record<NonNullable<HeadingProps["size"]>, string> = {
     sm: "text-xl md:text-2xl md:leading-snug",
     md: "text-3xl md:text-4xl md:leading-tight",
     xl: "text-4xl md:text-6xl md:leading-none",
     "2xl": "text-5xl md:text-7xl md:leading-none",
   };
+
   return (
     <Tag
       className={cn(
